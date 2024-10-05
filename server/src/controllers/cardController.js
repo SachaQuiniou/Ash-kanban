7d908ec4453f4e2ce32417c50201f944b21475c9
SachaQuiniou <sacha.quiniou@oclock.school>
2024-10-05 11:55:12 +0200

    [edit]-cardRouter, cardController

--- server/src/controllers/cardController.js
+++ server/src/controllers/cardController.js
@@ -1,10 +1,111 @@
-import { Card } from '../models/associations.js';
+import Joi from 'joi';
+import sanitize from 'sanitize-html';
+import { Card, List } from '../models/associations.js';
+import { col } from 'sequelize';
 
 export const cardController = {
     async index(req, res) {
         const cards = await Card.findAll({
-            include: [{ association: 'tags' }],
+            include: 'tags',
         });
         res.json(cards);
+    },
+
+    async show(req, res, next) {
+        const { id } = req.params;
+
+        const card = await Card.findByPk(id);
+
+        if (!card) {
+            return next();
+        }
+
+        res.json(card);
+    },
+
+    async store(req, res, next) {
+        const schema = Joi.object({
+            content: Joi.string().min(1).required(),
+            listId: Joi.number().integer().greater(0).required(),
+            position: Joi.number().integer().greater(0),
+            color: Joi.string(),
+        });
+
+        const { error } = schema.validate(req.body);
+        if (error) {
+            return next(error);
+        }
+
+        const { content, listId, position, color } = req.body;
+
+        const list = await List.findByPk(listId);
+        if (!list) {
+            return next();
+        }
+
+        const createdCard = await Card.create({
+            content: sanitize(content),
+            listId,
+            position,
+            color,
+        });
+
+        res.status(201).json(createdCard);
+    },
+
+    async update(req, res, next) {
+        const { id } = req.params;
+
+        const schema = Joi.object({
+            content: Joi.string().min(1),
+            listId: Joi.number().integer().greater(0),
+            position: Joi.number().integer().greater(0),
+            color: Joi.string(),
+        })
+            .min(1)
+            .message(
+                "Missing body parameters. Provide at least 'content' or 'position' or 'list_id' or 'color' properties"
+            );
+
+        const { error } = schema.validate(req.body);
+        if (error) {
+            return next(error);
+        }
+
+        const { content, listId, position, color } = req.body;
+
+        const card = await Card.findByPk(id);
+        if (!card) {
+            return res.status(404).json({ error: 'Card not found' });
+        }
+
+        if (listId) {
+            const list = await List.findByPk(listId);
+            if (!list) {
+                return next();
+            }
+        }
+
+        const updatedCard = await card.update({
+            content: sanitize(content),
+            listId,
+            position,
+            color,
+        });
+
+        res.json(updatedCard);
+    },
+
+    async destroy(req, res, next) {
+        const { id } = req.params;
+
+        const card = await Card.findByPk(id);
+        if (!card) {
+            return next();
+        }
+
+        await card.destroy();
+
+        res.status(204).end();
     },
 };
--- server/src/routers/cardRouter.js
+++ server/src/routers/cardRouter.js
@@ -5,3 +5,10 @@ import { catchErrors as ce } from '../middlewares/catchErrors.js';
 export const cardRouter = Router();
 
 cardRouter.get('/cards', ce(cardController.index));
+cardRouter.get('/cards/:id(\\d+)', ce(cardController.show));
+
+cardRouter.post('/cards', ce(cardController.store));
+
+cardRouter.patch('/cards/:id(\\d+)', ce(cardController.update));
+
+cardRouter.delete('/cards/:id(\\d+)', ce(cardController.destroy));
